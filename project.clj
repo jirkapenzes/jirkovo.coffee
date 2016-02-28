e00705d5172ed6b7086cef087163017d974df6e4
Jirka Pénzeš <jirkapenzes@gmail.com>
2016-02-28 21:54:12 +0100

    Add project description to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jirkovocoffee "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "jirkovo coffee | Káva, kavárny a život v nich.
+  :url "http://jirkovocoffee.cz"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [http-kit "2.1.18"]
